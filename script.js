8e4944bed65f85d9d78ef3a6ee32a75e67270ee6
alex-pyvovarov <127787680+alex-pyvovarov@users.noreply.github.com>
2023-06-20 08:52:32 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function doSomethingCool(){
 //var x ='Warning (but in a var);
 //alert ('Warning!');
-  var x = Math.random() * 10;
-  var y = Math.floor(x);
-  alert(y);
+  //var x = Math.random() * 10;
+  //var y = Math.floor(x);
+  var name = document.querySelector('#name').value;
+  alert(name + ' is Cool');
 }
